096700d096163103568f05ac1e13884893525c5d
Sosnus <sosnus15@gmail.com>
2021-06-21 07:36:24 +0200

    add subpage and models

--- apiconsument/lib/page_add_user.dart
+++ apiconsument/lib/page_add_user.dart
@@ -1,11 +1,16 @@
 import 'dart:convert';
 
+import 'package:apiconsument/data/roles_collection.dart';
 import 'package:apiconsument/single_post_page.dart';
 import 'package:chopper/chopper.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import 'data/api_service.dart';
+import 'data/role.dart';
+import 'data/user.dart';
+
+import 'package:flutter_datetime_picker/flutter_datetime_picker.dart';
 
 class PageAddUser extends StatefulWidget {
   PageAddUser({
@@ -50,6 +55,16 @@ class _PageAddUserState extends State<PageAddUser> {
   TextEditingController form_gender_EditingController = TextEditingController();
 
   TextEditingController form_pesel_EditingController = TextEditingController();
+
+  TextEditingController form_birthDate_EditingController =
+      TextEditingController();
+
+  TextEditingController form_roleId_EditingController = TextEditingController();
+
+  TextEditingController form_officeId_EditingController =
+      TextEditingController();
+
+  String dateTimeFromPicker = "todo";
 
   _formAddNewUser(BuildContext context) {
     // roleL
@@ -139,6 +154,17 @@ class _PageAddUserState extends State<PageAddUser> {
               return null;
             },
           ),
+          ElevatedButton(
+              onPressed: () {
+                // dateTimeFromPicker = await
+                selectDatetime();
+                print(dateTimeFromPicker);
+              },
+              child: Text(
+                'Select birth date: ' + dateTimeFromPicker,
+                // style: TextStyle(color: Colors.blue),
+              )),
+
           // ElevatedButton(
           //   onPressed: processingForm(context),
           //   child: Text('Submit'),
@@ -148,7 +174,9 @@ class _PageAddUserState extends State<PageAddUser> {
     );
   }
 
-  String dropdownValue = 'One';
+  // Role dropdownValue = RolesCollection.getRole(0);
+  String dropdownValue = RolesCollection.getRole(0).asString();
+  // String dropdownValue = 'One';
   Widget roleDropdownWidget() {
     return DropdownButton<String>(
       value: dropdownValue,
@@ -165,8 +193,12 @@ class _PageAddUserState extends State<PageAddUser> {
           dropdownValue = newValue!;
         });
       },
-      items: <String>['One', 'Two', 'Free', 'Four']
-          .map<DropdownMenuItem<String>>((String value) {
+      // items: DropdownMenuItem<String> RolesCollection.rolesListString();
+      // items: <String>['One', 'Two', 'Free', 'Four']
+      items: <String>[
+        RolesCollection.getRole(0).asString(),
+        RolesCollection.getRole(1).asString()
+      ].map<DropdownMenuItem<String>>((String value) {
         return DropdownMenuItem<String>(
           value: value,
           child: Text(value),
@@ -182,12 +214,29 @@ class _PageAddUserState extends State<PageAddUser> {
       ScaffoldMessenger.of(context)
           .showSnackBar(SnackBar(content: Text('Processing Data')));
     }
-    print(form_id_EditingController.text);
-    print(form_first_name_EditingController.text);
-    print(form_middle_name_EditingController.text);
-    print(form_surname_name_EditingController.text);
-    print(form_gender_EditingController.text);
-    print(form_pesel_EditingController.text);
+    User tempUser = new User(
+      //  form_id_EditingController.text,
+      form_first_name_EditingController.text,
+      form_middle_name_EditingController.text,
+      form_surname_name_EditingController.text,
+      form_pesel_EditingController.text,
+      form_gender_EditingController.text,
+      form_birthDate_EditingController.text,
+      int.parse(form_roleId_EditingController.text),
+      int.parse(form_officeId_EditingController.text),
+    );
+  }
+
+  selectDatetime() async {
+    return DatePicker.showDatePicker(context, showTitleActions: true,
+        onChanged: (date) {
+      print('change $date in time zone ' +
+          date.timeZoneOffset.inHours.toString());
+    }, onConfirm: (date) {
+      print('confirm $date');
+    },
+        currentTime: DateTime.utc(1990, 12, 31, 23, 12, 34),
+        locale: LocaleType.en);
   }
 }
 // }
